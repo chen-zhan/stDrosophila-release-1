e06a2301484ba1ea67be8c701270c2095f6bce85
Jiajun Yao <yjj_zj@163.com>
2022-01-14 21:32:42 +0800

    Add basic files

--- stDrosophila/plot/three_d_plot.py
+++ stDrosophila/plot/three_d_plot.py
@@ -28,15 +28,21 @@ def clip_3d_coords(adata: AnnData,
     clipped_grid = grid.clip_surface(surf)
     clipped_adata = adata[adata.obs.index.isin(clipped_grid["index"].tolist()), :]
 
+    clipped_points = pd.DataFrame()
+    clipped_points[0] = list(map(tuple, clipped_grid.points))
+    surf_points = list(map(tuple, surf.points.round(1)))
+    clipped_points[1] = clipped_points[0].isin(surf_points)
+    clipped_adata = clipped_adata[~clipped_points[1].values, :]
+
     return clipped_adata, surf
 
 
-def group_color(groups,
-                colormap: Union[str, list, dict] = "viridis",
-                alphamap: Union[float, list, dict] = 1.0,
-                mask_color: Optional[str] = "whitesmoke",
-                mask_alpha: Optional[float] = 0.5
-                ):
+def groups_color(groups,
+                 colormap: Union[str, list, dict] = "viridis",
+                 alphamap: Union[float, list, dict] = 1.0,
+                 mask_color: Optional[str] = "whitesmoke",
+                 mask_alpha: Optional[float] = 0.5
+                 ):
 
     color_groups = groups.unique().tolist()
     color_groups.sort()
@@ -63,19 +69,48 @@ def group_color(groups,
     return colordict
 
 
+def genes_color(genes_exp,
+                colormap: Union[str, list, dict] = "hot_r",
+                alphamap: Union[float, list, dict] = 1.0,
+                mask_color: Optional[str] = "whitesmoke",
+                mask_alpha: Optional[float] = 0.5):
+
+
+    color_genes = genes_exp.unique().tolist()
+    color_genes.sort()
+    colordict = {}
+
+    # Set gene color
+    if isinstance(colormap, str):
+        colormap = [mpl.colors.to_hex(i, keep_alpha=False) for i in
+                    sns.color_palette(palette=colormap, n_colors=len(color_genes), as_cmap=False)]
+    if isinstance(colormap, list):
+        colormap = {group: color for group, color in zip(color_genes, colormap)}
+    for gene in color_genes:
+        rgb_color = mpl.colors.to_rgb(colormap[gene])
+        colordict[gene] = [rgb_color[0], rgb_color[1], rgb_color[2], alphamap]
+    mask_rgb_color = mpl.colors.to_rgb(mask_color)
+    colordict[float(0)] = [mask_rgb_color[0], mask_rgb_color[1], mask_rgb_color[2], mask_alpha]
+    return colordict
+
+
 def build_3Dmodel(adata: AnnData,
                   coordsby: Optional[list] = None,
                   groupby: Optional[str] = "cluster",
                   group_show: Union[str, list] = "all",
+                  group_cmap: Union[str, list, dict] = "viridis",
+                  group_amap: Union[float, list, dict] = 1.0,
                   gene_show: Union[str, list] = "all",
-                  colormap: Union[str, list, dict] = "viridis",
-                  alphamap: Union[float, list, dict] = 1.0,
-                  mask_color: Optional[str] = "whitesmoke",
+                  gene_cmap: Union[str, list, dict] = "hot_r",
+                  gene_amap: Union[float, list, dict] = 1.0,
+                  mask_color: Optional[str] = "gainsboro",
                   mask_alpha: Optional[float] = 0.5,
                   smoothing: Optional[bool] = True,
-                  voxelize: Optional[bool] = True,
-                  unstructure: Optional[bool] = True
+                  voxelize: Optional[bool] = False,
+                  voxel_size: Optional[list] = None,
+                  unstructure: Optional[bool] = False
                   ):
+
     # Clip mesh using a pyvista.PolyData surface mesh.
     if smoothing:
         _adata, _ = clip_3d_coords(adata=adata, coordsby=coordsby)
@@ -91,10 +126,20 @@ def build_3Dmodel(adata: AnnData,
         groups = _adata.obs[groupby].map(lambda x: str(x) if x in group_show else "mask")
     else:
         raise ValueError("`group_show` value is wrong.")
-
     # Set group color(rgba)
-    colordict = group_color(groups=groups, colormap=colormap, alphamap=alphamap,
-                            mask_color=mask_color, mask_alpha=mask_alpha)
+    groups_rgba = groups_color(groups=groups, colormap=group_cmap, alphamap=group_amap,
+                               mask_color=mask_color, mask_alpha=mask_alpha)
+
+    # filter gene info
+    genes_exp = _adata.X.sum(axis=1) if gene_show == "all" else _adata[:, gene_show].X.sum(axis=1)
+    genes_data = pd.DataFrame([groups.values.tolist(), genes_exp.tolist()]).stack().unstack(0)
+    genes_data.columns = ["group", "genes_exp"]
+    genes_data["filter"] = genes_data[["group", "genes_exp"]].apply(
+        lambda x: 0 if x["group"] is "mask" else x["genes_exp"], axis=1).astype(float)
+    new_genes_exp = genes_data["filter"].round(5)
+    # Set gene color(rgba)
+    genes_rgba = genes_color(genes_exp=new_genes_exp, colormap=gene_cmap, alphamap=gene_amap,
+                             mask_color=mask_color, mask_alpha=mask_alpha)
 
     # Create a point cloud(pyvista.PolyData) or a voxelized volume(pyvista.UnstructuredGrid).
     if coordsby is None:
@@ -103,13 +148,21 @@ def build_3Dmodel(adata: AnnData,
     points_data = points_data.astype(float)
     points = PVGeo.points_to_poly_data(points_data)
     surface = points.delaunay_3d().extract_geometry()
-    mesh = PVGeo.filters.VoxelizePoints().apply(points) if voxelize else points
-    if unstructure and isinstance(mesh, pv.core.pointset.PolyData):
-        mesh = mesh.cast_to_unstructured_grid()
+
+    if voxelize is True:
+        voxelizer = PVGeo.filters.VoxelizePoints()
+        voxel_size = [1, 1, 1] if voxel_size is None else voxel_size
+        voxelizer.set_deltas(voxel_size[0], voxel_size[1], voxel_size[2])
+        voxelizer.set_estimate_grid(False)
+        mesh = voxelizer.apply(points)
+    else:
+        mesh = points.cast_to_unstructured_grid() if unstructure else points
+
     mesh["points_coords"] = points_data
-    mesh["genes_exp"] = _adata.X.sum(axis=1) if gene_show == "all" else _adata[:, gene_show].X.sum(axis=1)
-    mesh[groupby] = groups.values
-    mesh[f"{groupby}_rgba"] = np.array([colordict[g] for g in groups.tolist()])
+    mesh["genes_exp"] = new_genes_exp.values
+    mesh["genes_rgba"] = np.array([genes_rgba[g] for g in new_genes_exp.tolist()])
+    mesh["groups"] = groups.values
+    mesh["groups_rgba"] = np.array([groups_rgba[g] for g in groups.tolist()])
 
     return mesh, surface
 
@@ -135,34 +188,3 @@ def threeDslicing(mesh,
     else:
         # Create many slices of the input dataset along a specified axis.
         return mesh.slice_along_axis(n=n_slices, axis=axis, center=center)
-
-
-def easy_plot(surface=None, meshes=None, scalars=None, shape=(1, 1), cpos="iso", background_color="white", save=None):
-
-    if isinstance(meshes, list) is False:
-        meshes = [meshes]
-    if isinstance(shape, str):
-        n = re.split("[|/]", shape)
-        subplot_index_list = [i for i in range(int(n[0]) + int(n[1]))]
-    else:
-        subplot_index_list = [[i, j] for i in range(shape[0]) for j in range(shape[1])]
-
-    p = pv.Plotter(shape=shape)
-    p.background_color = background_color
-    for mesh, subplot_index in zip(meshes, subplot_index_list):
-        if isinstance(subplot_index, int):
-            p.subplot(subplot_index)
-        elif isinstance(subplot_index, list):
-            p.subplot(subplot_index[0], subplot_index[1])
-        if surface is not None:
-            p.add_mesh(surface, color="whitesmoke", opacity=0.2)
-        p.add_mesh(mesh, scalars=scalars, rgba=True)
-    p.camera_position = cpos
-    p.show(screenshot=save)
-
-
-
-
-
-
-
