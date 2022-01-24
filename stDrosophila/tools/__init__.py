6866bd3c5b6a832e0dc35bf7a2dc6eeb85840806
Jiajun Yao <yjj_zj@163.com>
2022-01-24 15:06:26 +0800

    update

--- stDrosophila/tools/__init__.py
+++ stDrosophila/tools/__init__.py
@@ -2,3 +2,4 @@ from .alignment import slice_alignment, slice_alignment_bigBin
 from .cluster import *
 from .anno import anno_flyatlas2
 from .genes_anno import genes_flyaltas2
+from .segmentation import find_nuclear_genes
